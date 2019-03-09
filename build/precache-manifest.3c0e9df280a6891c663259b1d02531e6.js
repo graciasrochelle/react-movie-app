bf2e2506ec6f35973a2a5e38a65c04dd09049b32
RMG <graciasrochelle@gmail.com>
2019-03-09 17:45:33 +1100

    build folder

---
+++ build/precache-manifest.3c0e9df280a6891c663259b1d02531e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3c970ec05720aeb2fb6",
+    "url": "/static/css/main.2fa4ffc9.chunk.css"
+  },
+  {
+    "revision": "a3c970ec05720aeb2fb6",
+    "url": "/static/js/main.a3c970ec.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "306664620c5488ad358a",
+    "url": "/static/css/2.5e2f0a86.chunk.css"
+  },
+  {
+    "revision": "306664620c5488ad358a",
+    "url": "/static/js/2.30666462.chunk.js"
+  },
+  {
+    "revision": "466ec83dac3001b5d3b3f74ad97287b2",
+    "url": "/static/media/loading.466ec83d.gif"
+  },
+  {
+    "revision": "465dc53c99fc5b3b383b4df50b9ba81f",
+    "url": "/static/media/logo.465dc53c.svg"
+  },
+  {
+    "revision": "57350b92f134ccf1dc2f756c1774e90f",
+    "url": "/index.html"
+  }
+];
